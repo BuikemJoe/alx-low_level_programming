c92f1c101316490e23b8c3dbe23e2d3dabf762dc
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-07-01 12:38:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Desc: To print the English alphabets in lowercase, followed by a new line
+ * Return: 0
+ */
+int main(x = 'A'; x <= 'Z'; x++)
+	putchar(tolower(x));
+	return (0);
