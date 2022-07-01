cf4e08d3c577f125692f33ab5ffbbd5f15df106e
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-07-01 15:58:43 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * Desc: printing of last digits of random numbers; and comparing them with a given set of numbers.
+ * Desc: printing of last digits of random input numbers.
  *
  * Return: 0 (success)
  */
@@ -18,9 +18,9 @@ int main(void)
 
 	if (last > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	else if(last == 0)
+	else if (last == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last);
-	else if(last < 6 && last != 0)
+	else if (last < 6 && last != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
