936b5da67c88cf881f7fa1a8711960ac78ae9113
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-07-05 21:09:06 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,19 +10,20 @@ int print_sign(int n)
 {
 	int value = 0;
 	{
-	if (n > 0)
-		value = 1;
-		_putchar('+');
-	}
-	else if (n == 0)
-	{
-		value = 0;
-		_putchar('0');
-	}
-	else if (n < 0)
-	{
-		value = -1;
-		_putchar('-');
-	}
+		if (n > 0)
+		{
+			value = 1;
+			_putchar('+');
+		}
+		else if (n == 0)
+		{
+			value = 0;
+			_putchar('0');
+		}
+		else if (n < 0)
+		{
+			value = -1;
+			_putchar('-');
+		}
 	return (value);
 }
