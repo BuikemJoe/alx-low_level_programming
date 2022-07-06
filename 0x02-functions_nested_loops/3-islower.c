41c8f1ae69027a4e79ea9acc4cf41a1849c87bdc
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-07-05 20:48:16 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,9 +10,9 @@ int _islower(int c)
 	int is_lower = 0;
 
 	for (i = 'a'; i <= 'z'; i++)
-		{
-			if (i == c)
-			is_lower = 1;
-		}
-		return (is_lower);
+	{
+		if (i == c)
+		is_lower = 1;
+	}
+	return (is_lower);
 }
