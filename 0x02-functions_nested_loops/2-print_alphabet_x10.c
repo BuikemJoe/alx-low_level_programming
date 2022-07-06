acd3d0e46f23ca887d891784eada00299fc43edb
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-07-05 19:59:29 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10X the alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+	return (0);
+}
