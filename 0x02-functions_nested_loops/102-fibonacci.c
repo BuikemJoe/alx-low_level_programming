529839a73919d4e5a6c83f97bb6518d204d4bb59
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-07-05 23:42:15 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,18 +14,16 @@ int main(void)
 	k = 2;
 	sum = 0;
 
-	for (i = 1; i <= 33; i++)
+	for (i = 1; i <= 50; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
 			sum = sum + j;
 		}
-		next = j + k;
-		j = k;
-		k = next;
+			next = j + k;
+			j = k;
+			k = next;
 	}
-
-	printf("%lu\n", sum);
-
+	printf("%lu\n", next);
 	return (0);
 }
