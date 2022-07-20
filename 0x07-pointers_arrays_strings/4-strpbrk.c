4874b5f3d4080eeceebe00bb878792ed397f4e27
Josephat Onyekwelu <onyekwelu.jo@gmail.com>
2022-07-19 17:17:02 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,26 +2,28 @@
 #include <stdio.h>
 
 /**
- * _strpbrk - bytes
- * @s: pointer to char
- * @accept: pointer to char
- * Return: NULL
+ * _strpbrk - matches any character specified
+ * @s: This is the C string to be scanned.
+ * @accept: character in str1 that matches one of the characters in str2
+ * Return: string s that matches any character specified in accept
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while (*s)
+	while while (*s != '\0')
 	{
-		for (i = 0; accept[i]; i++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (*s == accept[i])
+			if (*s == accept[j])
 			{
 				return (s);
 			}
+			j++;
 		}
 		s++;
 	}
-	return (NULL);
+	return (0);
 }
