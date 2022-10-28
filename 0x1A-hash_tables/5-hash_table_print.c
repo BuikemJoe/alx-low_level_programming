72d37b9e34eef17870fce8a67d7b65f196e1f194
Josephat Onyekwelu <106952334+BuikemJoe@users.noreply.github.com>
2022-10-28 06:24:28 +0100

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,18 +13,18 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	unsigned char comma_flag = 0;
 
-	if (hasht == NULL)
+	if (ht == NULL)
 		return;
 
 	printf("{");
-	for (i = 0; i < hasht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (hasht->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
 			if (comma_flag == 1)
 				printf(", ");
 
-			node = hasht->array[i];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				printf("'%s': '%s'", node->key, node->value);
